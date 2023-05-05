be38829493aad628063204b7810ab7e86e526d12
anasharma7 <117554760+anasharma7@users.noreply.github.com>
2023-05-05 08:42:04 -0400

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -31,7 +31,60 @@ public class BinarySearchTree {
      *         the values wasn't there)
      */
     public boolean swapNodes(int firstValue, int secondValue) {
-        return false;
+        Node[] nodes = findNodes(root, firstValue, secondValue);
+        if (nodes[0] == null || nodes[1] == null) {
+            return false;
+        }
+        int temp = nodes[0].data;
+        nodes[0].data = nodes[1].data;
+        nodes[1].data = temp;
+        return true;
+    }
+
+    private Node[] findNodes(Node current, int firstValue, int secondValue) {
+        if (current == null) {
+            return new Node[] { null, null };
+        }
+        if (current.data == firstValue) {
+            return new Node[] { current, findNode(root, secondValue) };
+        }
+        if (current.data == secondValue) {
+            return new Node[] { findNode(root, firstValue), current };
+        }
+        Node[] left = findNodes(current.left, firstValue, secondValue);
+        if (left[0] != null && left[1] != null) {
+            return left;
+        }
+        Node[] right = findNodes(current.right, firstValue, secondValue);
+        if (right[0] != null && right[1] != null) {
+            return right;
+        }
+        if (left[0] != null && left[1] == null) {
+            Node[] found = new Node[2];
+            found[0] = left[0];
+            found[1] = findNode(root, secondValue);
+            return found;
+        }
+        if (right[0] == null && right[1] != null) {
+            Node[] found = new Node[2];
+            found[0] = findNode(root, firstValue);
+            found[1] = right[1];
+            return found;
+        }
+        return new Node[] { null, null };
+    }
+
+    private Node findNode(Node current, int value) {
+        if (current == null) {
+            return null;
+        }
+        if (current.data == value) {
+            return current;
+        }
+        if (value < current.data) {
+            return findNode(current.left, value);
+        }
+        return findNode(current.right, value);
     }
 
     /**
@@ -42,7 +95,17 @@ public class BinarySearchTree {
      * @return true if the tree is valid and false if it isn't
      */
     public boolean isValid() {
-        return false;
+        return isSubtreeValid(root, null, null);
+    }
+
+    private boolean isSubtreeValid(Node current, Node min, Node max) {
+        if (current == null) {
+            return true;
+        }
+        if ((min != null && current.data <= min.data) || (max != null && current.data >= max.data)) {
+            return false;
+        }
+        return isSubtreeValid(current.left, min, current) && isSubtreeValid(current.right, current, max);
     }
 
     /**
@@ -52,7 +115,38 @@ public class BinarySearchTree {
      * @return true if the tree is valid and false if it isn't
      */
     public boolean fixTree() {
-        return false;
+        Node[] nodes = findSwappableNodes(root, null, null);
+        if (nodes[0] == null || nodes[1] == null) {
+            return false;
+        }
+        return swapNodes(nodes[0].data, nodes[1].data);
+    }
+
+    /**
+     * Recursive function to find the two nodes that violate the BST
+     *
+     * @param current the current node being checked
+     * @param prev the previous node in the traversal
+     * @param nodes the nodes that violate the BST (if any)
+     * @return the nodes that violate the BST (if any)
+     */
+    private Node[] findSwappableNodes(Node current, Node prev, Node[] nodes) {
+        if (current == null) {
+            return nodes;
+        }
+
+        nodes = findSwappableNodes(current.left, prev, nodes);
+
+        if (prev != null && current.data < prev.data) {
+            if (nodes[0] == null) {
+                nodes[0] = prev;
+            }
+            nodes[1] = current;
+        }
+
+        nodes = findSwappableNodes(current.right, current, nodes);
+
+        return nodes;
     }
 
     /**
